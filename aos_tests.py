832477b0d168b0c80f9e2377d3766de61ea9c3e9
vinitha-kaliraj <100327064+vinitha-kaliraj@users.noreply.github.com>
2022-03-16 15:09:47 -0700

    updated files

---
+++ aos_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from aos1 import aos_methods as methods
+
+
+class AosTestPositiveTestCase(unittest.TestCase):
+    @staticmethod
+    def test_create_new_user():
+        methods.set_up()
+        methods.create_new_user()
+        methods.log_in()
+        methods.log_out()
+        methods.main_logo()
+        methods.checking_homepage_texts()
+        methods.top_menu_clickable()
+        methods.contactus()
+        methods.tear_down()
